ed520c236dd9d1ed97b73f83865765913914c250
Li Guangming <leeful@gmail.com>
2020-04-22 21:02:30 +0800

    upgrade 1.1.1g

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-curl -O https://www.openssl.org/source/openssl-1.1.1d.tar.gz
-tar xf openssl-1.1.1d.tar.gz
-cd openssl-1.1.1d
+curl -O https://www.openssl.org/source/openssl-1.1.1g.tar.gz
+tar xf openssl-1.1.1g.tar.gz
+cd openssl-1.1.1g
 ../build_openssl_dist.sh ios
 ../build_openssl_dist.sh osx
-rm openssl-1.1.1d.tar.gz
+cd - && rm openssl-1.1.1g.tar.gz
