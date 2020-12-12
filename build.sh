a31e61743c65a7769e8f56b96e9b060ea4ef15c0
Li Guangming <leeful@gmail.com>
2020-12-12 08:41:21 +0800

    add 1.1.1i

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-curl -O https://www.openssl.org/source/openssl-1.1.1d.tar.gz
-tar xf openssl-1.1.1d.tar.gz
-cd openssl-1.1.1d
+curl -O https://www.openssl.org/source/openssl-1.1.1i.tar.gz
+tar xf openssl-1.1.1i.tar.gz
+cd openssl-1.1.1i
 ../build_openssl_dist.sh ios
 ../build_openssl_dist.sh osx
-rm openssl-1.1.1d.tar.gz
+rm openssl-1.1.1i.tar.gz
